0efc46c2d42bd7eb19f955db1a3274c6a54f090a
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-05-14 18:33:58 +0200

    prog: format string

--- lins.nim
+++ lins.nim
@@ -5,4 +5,4 @@ import lexers.plain_text
 let t_start = cpu_time()
 lex_file("plain_benchmark.txt")
 let t_diff_ms = (cpu_time() - t_start) * 1000
-echo "Lexing took \x1B[32m", format_float(t_diff_ms, ffDecimal, 1), "\x1B[0m ms."
+echo "Lexing took \x1B[1;32m", format_float(t_diff_ms, ffDecimal, 1), "\x1B[0m ms."
