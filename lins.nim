dc34adf22b5d1c4caa241fbe50879606efe83894
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-05-15 23:00:34 +0200

    rules: implement lots of rules

--- lins.nim
+++ lins.nim
@@ -5,9 +5,16 @@ import rules.rules
 
 let RULES = @[
    RuleExistence.new(WARNING, "Consider removing '$1'.", "rules.txt", r"\b(wisdom|😇|Particular life)\b", true),
-   RuleExistence.new(ERROR, "Consider removing '$1'.", "rules.txt", "😇", true),
-   RuleExistence.new(ERROR, "Consider removing '$1'.", "rules.txt", r"\b(Basic pattern)\b", true),
-   RuleExistence.new(ERROR, "Consider removing '$1'.", "rules.txt", r"\b(King|county)\b", true)
+   RuleSubstitution.new(WARNING, "Consider using '$1' instead of '$2'.", "rules.txt", r"\bMarcus\b", "Mr. Eriksson", true),
+   RuleOccurrence.new(WARNING, "More than 10 occurrences of the word 'me'.",
+                      "rules.txt", r"\bme\b", 10, MAX, Scope.PARAGRAPH, true),
+   RuleRepetition.new(WARNING, "Foo is repeated.", "rules.txt", r"\bfoo\b",
+                      Scope.SENTENCE, true),
+   RuleConsistency.new(WARNING, "Inconsistent spelling '$1'.", "rules.txt", r"\bfoo\b",
+                       r"\bbar\b", Scope.TEXT, true),
+   RuleDefinition.new(WARNING, "'$1' has no definition.", "rules.txt",
+                       r"(?:\b[A-Z][a-z]+ )+\(([A-Z]{3,5})\)",
+                       r"\b([A-Z]{3,5})\b", @["UV"], Scope.TEXT, false)
 ]
 
 proc callback(s: Sentence) =
@@ -17,6 +24,6 @@ proc callback(s: Sentence) =
          echo v.severity_str, ": ", v.message
 
 let t_start = cpu_time()
-lex_file("plain_benchmark.txt", callback)
+lex_file("test_guardian.txt", callback)
 let t_diff_ms = (cpu_time() - t_start) * 1000
 echo "Lexing took \x1B[1;32m", format_float(t_diff_ms, ffDecimal, 1), "\x1B[0m ms."
--- rules/rules.nim
+++ rules/rules.nim
@@ -1,6 +1,7 @@
 import nre
 import unicode
 import strutils
+import tables
 
 import ../lexers/plain_text
 
@@ -14,6 +15,10 @@ type
       TEXT
       SENTENCE
       PARAGRAPH
+
+   Limit* = enum
+      MIN
+      MAX
 
    Position = tuple
       row, col: int
@@ -31,6 +36,45 @@ type
       severity: Severity
       message: string
       source_file: string
+
+   RuleExistence* = ref object of Rule
+      regex: Regex
+
+   RuleSubstitution* = ref object of Rule
+      regex: Regex
+      subst: string
+
+   RuleOccurrence* = ref object of Rule
+      regex: Regex
+      limit_val: int
+      limit_kind: Limit
+      scope: Scope
+      nof_matches: int
+      par_prev: int
+      has_alerted: bool
+
+   RuleRepetition* = ref object of Rule
+      regex: Regex
+      scope: Scope
+      par_prev: int
+      nof_matches: int
+
+   RuleConsistency* = ref object of Rule
+      regex_first: Regex
+      regex_second: Regex
+      scope: Scope
+      par_prev: int
+      nof_matches_first: int
+      nof_matches_second: int
+
+   RuleDefinition* = ref object of Rule
+      regex_def: Regex
+      regex_decl: Regex
+      exceptions: seq[string]
+      scope: Scope
+      definitions: Table[string, Position]
+      par_prev: int
+
 
 # Constructors
 proc new*(t: typedesc[Rule], kind: string, severity: Severity, message: string,
@@ -53,7 +97,8 @@ proc create_severity_string(r: Rule): string =
    return tmp
 
 # TODO: Fix type of message args and format the message with the arguments.
-proc create_violation(r: Rule, pos: Position, message_args: varargs[string]): Violation =
+proc create_violation(r: Rule, pos: Position,
+                      message_args: varargs[string]): Violation =
    (kind: r.kind, severity: r.severity,
     severity_str: r.create_severity_string(), source_file: r.source_file,
     message: format(r.message, message_args), position: pos)
@@ -83,13 +128,8 @@ proc calculate_position(r: Rule, row_begin, col_begin: int,
       return (row_begin + i, col - offset_closest_newline)
 
 # TODO: Raise exception
-proc enforce*(r: Rule): seq[Violation] =
+method enforce*(r: Rule, sentence: Sentence): seq[Violation] {.base.}  =
    echo "ENFORCE NOT IMPLEMENTED FOR RULE!", r.kind
-
-# Rule 'existence'
-type
-   RuleExistence* = ref object of Rule
-      regex: Regex
 
 proc new*(t: typedesc[RuleExistence], severity: Severity, message: string,
           source_file: string, regex: string, ignore_case: bool): RuleExistence =
@@ -98,9 +138,10 @@ proc new*(t: typedesc[RuleExistence], severity: Severity, message: string,
       regex_flags = "(?i)"
 
    return RuleExistence(kind: "existence", severity: severity, message: message,
-                        source_file: source_file, regex: re(regex_flags & regex))
+                        source_file: source_file,
+                        regex: re(regex_flags & regex))
 
-proc enforce*(r: RuleExistence, sentence: Sentence): seq[Violation] =
+method enforce*(r: RuleExistence, sentence: Sentence): seq[Violation] =
    var violations: seq[Violation] = @[]
 
    for m in nre.find_iter($sentence.str, r.regex):
@@ -113,3 +154,294 @@ proc enforce*(r: RuleExistence, sentence: Sentence): seq[Violation] =
 
    return violations
 
+
+proc new*(t: typedesc[RuleSubstitution], severity: Severity, message: string,
+          source_file: string, regex: string, subst: string,
+          ignore_case: bool): RuleSubstitution =
+   var regex_flags = ""
+   if ignore_case:
+      regex_flags = "(?i)"
+
+   return RuleSubstitution(kind: "substitution", severity: severity,
+                           message: message, source_file: source_file,
+                           regex: re(regex_flags & regex),
+                           subst: subst)
+
+
+method enforce*(r: RuleSubstitution, sentence: Sentence): seq[Violation] =
+   var violations: seq[Violation] = @[]
+
+   for m in nre.find_iter($sentence.str, r.regex):
+      let violation_pos = r.calculate_position(sentence.row_begin,
+                                               sentence.col_begin,
+                                               m.match_bounds.a + 1,
+                                               sentence.newlines)
+
+      violations.add(r.create_violation(violation_pos, r.subst, $m))
+
+   return violations
+
+proc new*(t: typedesc[RuleOccurrence], severity: Severity, message: string,
+          source_file: string, regex: string, limit_val: int, limit_kind: Limit,
+          scope: Scope, ignore_case: bool): RuleOccurrence =
+   var regex_flags = ""
+   if ignore_case:
+      regex_flags = "(?i)"
+
+   return RuleOccurrence(kind: "occurrence",
+                         severity: severity,
+                         message: message,
+                         source_file: source_file,
+                         regex: re(regex_flags & regex),
+                         limit_val: limit_val,
+                         limit_kind: limit_kind,
+                         scope: scope,
+                         nof_matches: 0,
+                         par_prev: 0,
+                         has_alerted: false)
+
+
+method enforce*(r: RuleOccurrence, sentence: Sentence): seq[Violation] =
+   var violations: seq[Violation] = @[]
+
+   # Reset the match counter and alert status depending on the scope.
+   case r.scope
+   of SENTENCE:
+      r.nof_matches = 0
+      r.has_alerted = false
+   of PARAGRAPH:
+      if not (r.par_prev == sentence.par_idx):
+         r.nof_matches = 0
+         r.has_alerted = false
+   else:
+      discard
+
+   for m in nre.find_iter($sentence.str, r.regex):
+      # Count the match (pre-incrementation).
+      r.nof_matches += 1
+
+      # Check against the specified maximum limit. If the value is out of
+      # bounds, we create a violation object, set the alert status and
+      # break to avoid unnecessary loop iterations.
+      if (not r.has_alerted and
+          (r.limit_kind == MAX) and (r.nof_matches > r.limit_val)):
+         let sentence_pos = (sentence.row_begin, sentence.col_begin)
+         violations.add(r.create_violation(sentence_pos))
+         break
+
+   # Check against the specified minimum limit. This is only supported in
+   # the sentence scope.
+   if (not r.has_alerted and
+       (r.limit_kind == MIN) and (r.nof_matches < r.limit_val)):
+      let sentence_pos = (sentence.row_begin, sentence.col_begin)
+      violations.add(r.create_violation(sentence_pos))
+
+   # Remember the paragraph.
+   r.par_prev = sentence.par_idx
+
+   return violations
+
+
+proc new*(t: typedesc[RuleRepetition], severity: Severity, message: string,
+          source_file: string, regex: string, scope: Scope,
+          ignore_case: bool): RuleRepetition =
+   var regex_flags = ""
+   if ignore_case:
+      regex_flags = "(?i)"
+
+   return RuleRepetition(kind: "repetition",
+                         severity: severity,
+                         message: message,
+                         source_file: source_file,
+                         regex: re(regex_flags & regex),
+                         scope: scope,
+                         nof_matches: 0,
+                         par_prev: 0)
+
+
+method enforce*(r: RuleRepetition, sentence: Sentence): seq[Violation] =
+   var violations: seq[Violation] = @[]
+
+   case r.scope
+   of SENTENCE:
+      r.nof_matches = 0
+   of PARAGRAPH:
+      if not (r.par_prev == sentence.par_idx):
+         r.nof_matches = 0
+   else:
+      discard
+
+   for m in nre.find_iter($sentence.str, r.regex):
+      r.nof_matches += 1
+
+      if (r.nof_matches > 1):
+         let violation_pos = r.calculate_position(sentence.row_begin,
+                                                  sentence.col_begin,
+                                                  m.match_bounds.a + 1,
+                                                  sentence.newlines)
+
+         violations.add(r.create_violation(violation_pos, $m))
+
+   # Remember the paragraph.
+   r.par_prev = sentence.par_idx
+
+   return violations
+
+
+proc new*(t: typedesc[RuleConsistency], severity: Severity, message: string,
+          source_file: string, regex_first: string, regex_second: string,
+          scope: Scope, ignore_case: bool): RuleConsistency =
+   var regex_flags = ""
+   if ignore_case:
+      regex_flags = "(?i)"
+
+   return RuleConsistency(kind: "consistency",
+                         severity: severity,
+                         message: message,
+                         source_file: source_file,
+                         regex_first: re(regex_flags & regex_first),
+                         regex_second: re(regex_flags & regex_second),
+                         scope: scope,
+                         nof_matches_first: 0,
+                         nof_matches_second: 0,
+                         par_prev: 0)
+
+
+method enforce*(r: RuleConsistency, sentence: Sentence): seq[Violation] =
+   var violations: seq[Violation] = @[]
+
+   # Reset the match counter and alert status depending on the scope.
+   case r.scope
+   of SENTENCE:
+      r.nof_matches_first = 0
+      r.nof_matches_second = 0
+   of PARAGRAPH:
+      if not (r.par_prev == sentence.par_idx):
+         r.nof_matches_first = 0
+         r.nof_matches_second = 0
+   else:
+      discard
+
+   # TODO: Fix implementation to be able to resolve in-sentence occurrences of
+   # both expressions. Which one is first etc.
+
+   # Analyze matches for the first regex.
+   for m in nre.find_iter($sentence.str, r.regex_first):
+      r.nof_matches_first += 1
+
+      if (r.nof_matches_second > 0):
+         let violation_pos = r.calculate_position(sentence.row_begin,
+                                                  sentence.col_begin,
+                                                  m.match_bounds.a + 1,
+                                                  sentence.newlines)
+
+         violations.add(r.create_violation(violation_pos, $m))
+
+   # Analyze matches for the second regex.
+   for m in nre.find_iter($sentence.str, r.regex_second):
+      r.nof_matches_second += 1
+
+      if (r.nof_matches_first > 0):
+         let violation_pos = r.calculate_position(sentence.row_begin,
+                                                  sentence.col_begin,
+                                                  m.match_bounds.a + 1,
+                                                  sentence.newlines)
+
+         violations.add(r.create_violation(violation_pos, $m))
+
+   # Remember the paragraph.
+   r.par_prev = sentence.par_idx
+
+   return violations
+
+# Regexes should be auto-filled on an above level. Drafts are:
+#   regex_def = r'(?:\b[A-Z][a-z]+ )+\(([A-Z]{3,5})\)'
+#   regex_decl = r'\b([A-Z]{3,5})\b'
+proc new*(t: typedesc[RuleDefinition], severity: Severity, message: string,
+          source_file: string, regex_def: string, regex_decl: string,
+          exceptions: seq[string], scope: Scope,
+          ignore_case: bool): RuleDefinition =
+   var regex_flags = ""
+   if ignore_case:
+      regex_flags = "(?i)"
+
+   return RuleDefinition(kind: "definition",
+                         severity: severity,
+                         message: message,
+                         source_file: source_file,
+                         regex_def: re(regex_flags & regex_def),
+                         regex_decl: re(regex_flags & regex_decl),
+                         exceptions: exceptions,
+                         scope: scope,
+                         definitions: init_table[string, Position](),
+                         par_prev: 0)
+
+
+method enforce*(r: RuleDefinition, sentence: Sentence): seq[Violation] =
+   var violations: seq[Violation] = @[]
+
+   # Reset the match counter and alert status depending on the scope.
+   case r.scope
+   of SENTENCE:
+      r.definitions.clear()
+   of PARAGRAPH:
+      if not (r.par_prev == sentence.par_idx):
+         r.definitions.clear()
+   else:
+      discard
+
+   # Go through the sentence looking for definitions. Store the position to
+   # make sure we can differentiate the order of definitions and
+   # declarations within a sentence.
+   for m_def in nre.find_iter($sentence.str, r.regex_def):
+      try:
+         let def = m_def.captures[0]
+         let pos = r.calculate_position(sentence.row_begin,
+                                        sentence.col_begin,
+                                        m_def.capture_bounds[0].get.a + 1,
+                                        sentence.newlines)
+
+         if r.definitions.has_key_or_put(def, pos):
+            echo "Redefinition of '", def, "'"
+         else:
+            echo "Defining '", def, "'"
+
+      except IndexError:
+         echo "ERROR no capture group for definition."
+
+
+   # Run through the sentence looking for declarations. If the declaration
+   # has no definition, the rule is violated outright. Otherwise, we have
+   # to double-check the position of the declaration in relation to the
+   # definition. We skip any declarations in the exception list.
+   for m_decl in nre.find_iter($sentence.str, r.regex_decl):
+      try:
+         let decl = m_decl.captures[0]
+         if decl in r.exceptions:
+            continue
+
+         let (row_decl, col_decl) =
+            r.calculate_position(sentence.row_begin, sentence.col_begin,
+                                 m_decl.capture_bounds[0].get.a + 1,
+                                 sentence.newlines)
+
+         var is_violated = false
+         if not r.definitions.has_key(decl):
+            is_violated = true
+         else:
+            let (row_def, col_def) = r.definitions[decl]
+
+            if (row_def == row_decl and col_def > col_decl or
+                row_def > row_decl):
+               is_violated = true
+
+         if is_violated:
+            # TODO: Fix the Position type, maybe a raw tuple instead
+            # let pos: Position = (row: row_decl, col: col_decl)
+            violations.add(r.create_violation((row_decl, col_decl), decl))
+
+
+      except IndexError:
+         echo "ERROR no capture group for definition."
+
+   return violations
