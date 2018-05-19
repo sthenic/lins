bb4a548eaf17ae8f7cc8c476b54de9312676a8dd
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-05-19 17:59:43 +0200

    utils: export logging procs

--- utils/log.nim
+++ utils/log.nim
@@ -1,15 +1,15 @@
 import strutils
 
-template info(msg: string, args: varargs[string]) =
-   echo format("\x1B[1;34m INFO: \x1B[0m" & msg, args)
+template info*(msg: string, args: varargs[string]) =
+   echo format("\x1B[1;34mINFO: \x1B[0m" & msg, args)
 
-template warning(msg: string, args: varargs[string]) =
-   echo format("\x1B[1;33m WARNING: \x1B[0m" & msg, args)
+template warning*(msg: string, args: varargs[string]) =
+   echo format("\x1B[1;33mWARNING: \x1B[0m" & msg, args)
 
-template error(msg: string, args: varargs[string]) =
-   echo format("\x1B[1;31m ERROR: \x1B[0m" & msg, args)
+template error*(msg: string, args: varargs[string]) =
+   echo format("\x1B[1;31mERROR: \x1B[0m" & msg, args)
 
-template debug(msg: string, args: varargs[string]) =
+template debug*(msg: string, args: varargs[string]) =
    when not defined(release):
-      echo format("\x1B[1;35m DEBUG: \x1B[0m" & msg, args)
+      echo format("\x1B[1;35mDEBUG: \x1B[0m" & msg, args)
 
