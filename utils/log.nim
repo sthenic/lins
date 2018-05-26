57f8acd868cc3985a6b6ed67f8af7d3be6c90b26
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-05-26 11:18:26 +0200

    [utils] Formatting.

--- utils/log.nim
+++ utils/log.nim
@@ -1,10 +1,13 @@
 import strutils
 
+
 template info*(msg: string, args: varargs[string]) =
    echo format("\x1B[1;34mINFO: \x1B[0m" & msg, args)
 
+
 template warning*(msg: string, args: varargs[string]) =
    echo format("\x1B[1;33mWARNING: \x1B[0m" & msg, args)
+
 
 template error*(msg: string, args: varargs[string]) =
    echo format("\x1B[1;31mERROR: \x1B[0m" & msg, args)
@@ -12,6 +15,7 @@ template error*(msg: string, args: varargs[string]) =
 template debug*(msg: string, args: varargs[string]) =
    when not defined(release):
       echo format("\x1B[1;35mDEBUG: \x1B[0m" & msg, args)
+
 
 template abort*(e: typedesc[Exception], msg: string, args: varargs[string]) =
    error(msg, args)
