c2a9b2da6ef3fcf38f7ac7e59eba108c1ac50301
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-05-28 22:15:37 +0200

    [utils] Align log messages.

--- utils/log.nim
+++ utils/log.nim
@@ -2,7 +2,7 @@ import strutils
 
 
 template info*(msg: string, args: varargs[string]) =
-   echo format("\x1B[1;34mINFO: \x1B[0m" & msg, args)
+   echo format("\x1B[1;34mINFO:    \x1B[0m" & msg, args)
 
 
 template warning*(msg: string, args: varargs[string]) =
@@ -10,12 +10,12 @@ template warning*(msg: string, args: varargs[string]) =
 
 
 template error*(msg: string, args: varargs[string]) =
-   echo format("\x1B[1;31mERROR: \x1B[0m" & msg, args)
+   echo format("\x1B[1;31mERROR:   \x1B[0m" & msg, args)
 
 
 template debug*(msg: string, args: varargs[string]) =
    when not defined(release):
-      echo format("\x1B[1;35mDEBUG: \x1B[0m" & msg, args)
+      echo format("\x1B[1;35mDEBUG:   \x1B[0m" & msg, args)
 
 
 template abort*(e: typedesc[Exception], msg: string, args: varargs[string]) =
