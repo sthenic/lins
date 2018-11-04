b55f56ea1847944ee7c88089e1b5fc9d9053c5e2
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-11-04 21:45:00 +0100

    [parsers] Very early draft of the parser.

--- src/parsers/latex_parser.nim
+++ src/parsers/latex_parser.nim
@@ -1,8 +1,24 @@
+import streams
 import strutils
 
 import ../lexers/tex_lexer
+
 
 type
    LaTeXParser = object
       lex*: TeXLexer
       tok*: TeXToken
+
+
+proc get_token*(p: var LaTeXParser) =
+   ## Get the next token from the lexer and store it in the `tok` member.
+   tex_lexer.get_token(p.lex, p.tok)
+
+
+proc open_parser*(p: var LaTeXParser, filename: string, s: Stream) =
+   init(p.tok)
+   open_lexer(p.lex, filename, s)
+
+
+proc close_parser*(p: var LaTeXParser) =
+   close_lexer(p.lex)
