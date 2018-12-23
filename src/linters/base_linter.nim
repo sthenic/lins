e8c410df02cb26614e2a6baf76645681926489fc
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2018-12-23 15:07:16 +0100

    [linters] Improve word_wrap to respect indentation levels.

--- src/linters/base_linter.nim
+++ src/linters/base_linter.nim
@@ -46,7 +46,13 @@ proc inc*(x: var ViolationCount, y: ViolationCount) =
    inc(x.suggestion, y.suggestion)
 
 
-# Borrowed implementation from Nim/devel until these are released.
+# Borrowed/improved word_wrapping implementation from Nim/devel until these are
+# released.
+type WordWrapState = enum
+   AfterNewline
+   MiddleOfLine
+
+
 proc olen(s: string): int =
    var i = 0
    while i < s.len:
@@ -55,50 +61,74 @@ proc olen(s: string): int =
       inc(i, L)
 
 
-# Above implementation is not perfect. Improve the implementation below.
 proc wrap_words*(s: string, max_line_width = 80, split_long_words = true,
                  seps: set[char] = Whitespace,
                  new_line = "\n"): string {.noSideEffect.} =
    ## Word wraps `s`.
    result = new_string_of_cap(s.len + s.len shr 6)
+   var state: WordWrapState
    var space_rem = max_line_width
-   var last_sep = ""
+   var last_sep, indent = ""
    for word, is_sep in tokenize(s, seps):
       let wlen = olen(word)
       if is_sep:
          # Process the whitespace 'word', adding newlines as needed and keeping
-         # any trailing non-newline whitespace characters.
+         # any trailing non-newline whitespace characters as the indentation
+         # level.
          for c in word:
             if c in NewLines:
                add(result, new_line)
                last_sep.set_len(0)
+               indent.set_len(0)
                space_rem = max_line_width
+               state = AfterNewline
             else:
-               add(last_sep, c)
-               dec(space_rem) # TODO: Treat tabs differently?
-         continue
+               case state
+               of AfterNewline:
+                  add(indent, c)
+                  space_rem = max_line_width - len(indent)
+               of MiddleOfLine:
+                  add(last_sep, c)
+                  dec(space_rem) # TODO: Treat tabs differently?
       elif wlen > space_rem:
-         if split_long_words and wlen > max_line_width:
-            result.add(last_sep)
-            last_sep.set_len(0)
+         if split_long_words and wlen > max_line_width - len(indent):
+            case state
+            of AfterNewline:
+               result.add(indent)
+            of MiddleOfLine:
+               result.add(last_sep)
+               last_sep.set_len(0)
+
             var i = 0
             while i < word.len: # TODO: Is word.len correct here?
                if space_rem <= 0:
-                  space_rem = max_line_width
-                  result.add(new_line)
+                  space_rem = max_line_width - len(indent)
+                  result.add(new_line & indent)
                dec(space_rem)
                let L = grapheme_len(word, i)
                for j in 0..<L:
                   result.add(word[i+j])
                inc(i, L)
          else:
-            space_rem = max_line_width - len(word)
-            result.add(new_line & last_sep & word)
+            space_rem = max_line_width - len(indent) - len(word)
+            result.add(new_line & indent & word)
             last_sep.set_len(0)
+
+         # TODO: Is this ok in the case when the word get broken to exactly 80 chars?
+         state = MiddleOfLine
       else:
+         # TODO: Think about what happens to space_rem if AfterNewLine. Is it
+         # already decremented with the indent level?
+         case state
+         of AfterNewline:
+            result.add(indent)
+         of MiddleOfLine:
+            result.add(last_sep)
+            last_sep.set_len(0)
+
          space_rem = space_rem - len(word)
-         result.add(last_sep & word)
-         last_sep.set_len(0)
+         result.add(word)
+         state = MiddleOfLine
 
 
 proc print_violation*(l: BaseLinter, v: Violation) =
