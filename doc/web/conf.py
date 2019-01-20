e1a62cab00929393f64d1b8ba2f856fa78b4f42f
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2019-01-20 16:24:40 +0100

    [docs] Revert back to using the Alabaster theme.

--- doc/web/conf.py
+++ doc/web/conf.py
@@ -69,52 +69,9 @@ pygments_style = 'sphinx'
 # documentation.
 #
 html_theme_options = {
-    # Specify a list of menu in Header.
-    # Tuples forms:
-    #  ('Name', 'external url or path of pages in the document', boolean, 'icon name')
-    #
-    # Third argument:
-    # True indicates an external link.
-    # False indicates path of pages in the document.
-    #
-    # Fourth argument:
-    # Specify the icon name.
-    # For details see link.
-    # https://material.io/icons/
-    'header_links' : [
-        ('Home', 'index', False, 'home'),
-        ("ExternalLink", "http://example.com", True, 'launch'),
-        ("NoIconLink", "http://example.com", True, ''),
-        ("GitHub", "https://github.com/myyasuda/sphinx_materialdesign_theme", True, 'link')
-    ],
-
-    # Customize css colors.
-    # For details see link.
-    # https://getmdl.io/customize/index.html
-    #
-    # Values: amber, blue, brown, cyan deep_orange, deep_purple, green, grey, indigo, light_blue,
-    #         light_green, lime, orange, pink, purple, red, teal, yellow(Default: indigo)
-    'primary_color': 'indigo',
-    # Values: Same as primary_color. (Default: pink)
-    'accent_color': 'blue',
-
-    # Customize layout.
-    # For details see link.
-    # https://getmdl.io/components/index.html#layout-section
-    'fixed_drawer': False,
-    'fixed_header': True,
-    'header_waterfall': True,
-    'header_scroll': False,
-
-    # Render title in header.
-    # Values: True, False (Default: False)
-    'show_header_title': False,
-    # Render title in drawer.
-    # Values: True, False (Default: True)
-    'show_drawer_title': True,
-    # Render footer.
-    # Values: True, False (Default: True)
-    'show_footer': True
+    'description': 'A lightweight, extensible linter for prose.',
+    'fixed_sidebar': True,
+    'logo': 'logo.png'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -138,4 +95,4 @@ html_sidebars = {
         'donate.html',
     ]
 }
-html_theme = 'sphinx_materialdesign_theme'
+html_theme = 'alabaster'
